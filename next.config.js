f6c65cf4fd02432d5bf513735a5cd4bc14589904
lesha358 <lesha358@github.com>
2025-03-23 13:26:47 +0300

    Обновлена конфигурация Next.js для локальных изображений

--- next.config.js
+++ next.config.js
@@ -1,18 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'static.tildacdn.com',
-        pathname: '/**',
-      }
-    ],
     unoptimized: true
   },
   output: 'standalone'
